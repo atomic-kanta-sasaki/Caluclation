9ff7cae4e24d75b39aabe1558f2b1c907c4a6db1
kanta_sasaki <k_padbjw@icloud.com>
2020-04-03 18:04:53 +0900

    first commit

---
+++ swift_practice.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+let age = 17
+switch age {
+case 0...6:
+    print("kindergaden kid")
+case 7...12:
+    print("primary school student")
+case 13...15:
+    print("junior high student")
+case 16...18:
+    print("high school student")
+}
+
